d6d866ec69e9ee532c12ed31ac51ed60ce750193
sxr10188 <43756714+sxr10188@users.noreply.github.com>
2018-11-13 12:24:47 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,23 @@
+export GITHUB_CLIENT_ID=abcd1234abcd1234abcd   
+export GITHUB_CLIENT_SECRET=abcd1234abcd5678abcd1234abcd5678abcd1234
+export APP_SECRET_KEY=abcd1234
+export GITHUB_ORG=ucsd-cse-spis-2017
+
+# Keep this comment and the ones below.
+# To use this file, type this at the bash shell prompt:
+#
+#     . env.sh
+# 
+# Yes, the space between . and env.sh is SUPPOSED to be there.
+# To see if it worked, type:
+#
+#    echo $GITHUB_CLIENT_ID
+#    echo $GITHUB_CLIENT_SECRET 
+#    etc.
+#
+# You need to do this each time you open a new terminal session
+# before running your webapp with 
+#  
+#   python webapp.py
+#
+# or before starting idle
